a6a1288ecf7f49f5420c336a2ab383c4797b0056
WMinerva <nicolas.ochoa1697@gmail.com>
2024-01-18 02:13:12 -0400

    show & hide

--- show-hide-title/src/App.js
+++ show-hide-title/src/App.js
@@ -1,5 +1,18 @@
-import "./App.css";
+import React, { useState } from "react";
 
-function App() {}
+const App = () => {
+  const [mostrarParrafo, setMostrarParrafo] = useState(false);
+
+  const toggleParrafo = () => {
+    setMostrarParrafo(!mostrarParrafo);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleParrafo}>Mostrar/ocultar párrafo</button>
+      {mostrarParrafo && <p>Este es el párrafo que aparece y desaparece</p>}
+    </div>
+  );
+};
 
 export default App;
